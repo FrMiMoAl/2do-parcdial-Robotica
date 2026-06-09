1f30b791a3f1af89d1a24dd9a18babf58225d7a8
FrMiMoAl <32112849+FrMiMoAl@users.noreply.github.com>
2026-06-09 11:09:34 -0400

    Delete Ejer_3.1/src directory

--- Ejer_3.1/src/share/ejercicio3_rl_navigation/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/franco/Desktop/Robotica/2dopar/build/ejercicio3_rl_navigation/src"
